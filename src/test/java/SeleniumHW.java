afe95b680d7e1f3ae102767712b211dfc6a02bf2
aleksandraurchenko <alex_bloom@mail.ru>
2022-08-07 09:06:17 +0300

    initial commit

---
+++ src/test/java/SeleniumHW.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumHW {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void ShouldSendForm() {
+        driver.get("http://localhost:7777");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Юлия Цой-Цой");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79018765647");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
+}
